ae9fdb30281ea2a355cb9c2107a10523800cff69
Kelsey Lawrence <kelseylawrence@Kelseys-MacBook-Pro.local>
2013-06-21 12:37:34 -0700

    php added, form sending
    madlib sends with user input in place

---
+++ form-process.php
@@ -0,0 +1,20 @@
+<?php
+
+	$adj1 = $_POST["adjective1"];
+	$plnoun = $_POST["pluralnoun"];
+	$bodpart = $_POST["partofthebody"];
+	$adj2 = $_POST["adjective2"];
+	$adj3 = $_POST["adjective3"];
+
+	var_dump($_POST);
+
+	$message = "According to a/an ".$adj1." survey, more Americans go to zoos than watch ".$plnoun." on TV or play games such as ".$bodpart."-ball. Zoos have every kind of animal, from ".$adj2." aardvarks to ".$adj3." zebras. The most fun to watch are the monkeys.";
+
+	$message = wordwrap($message, 70, "\r\n");
+
+	$headers = "From: nobody@whocares.com";
+
+	echo mail("kelshaelaw@gmail.com", "a Madlib for you to have", $message, $headers);
+
+
+?>
